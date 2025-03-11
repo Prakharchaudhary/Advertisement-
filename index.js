dc6b44ba5bb4a23b5db667e80ba17463b8ef148b
prakharchaudhary <cprakhar999@gmail.com>
2025-03-11 14:41:43 +0530

    cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: "Content-Type,Authorization" }));
 
 // Connect Database
 connectDB();
